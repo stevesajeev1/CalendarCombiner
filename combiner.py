0f291b019bd0a99cfeedef559b8a1749bd22d295
Steve Sajeev <stevesajeev123@gmail.com>
2025-01-08 10:05:57 -0500

    Combine events that are shared

--- combiner.py
+++ combiner.py
@@ -1,30 +1,51 @@
 import os
 import icalendar
+from util import EventWrapper
 
 # Add more categories as needed
-OUTLOOK_EVENT_CATEGORIES = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple']
+OUTLOOK_EVENT_CATEGORIES = ['Red category', 'Orange category', 'Yellow category', 'Green category', 'Blue category', 'Purple category']
+
+
 
 # Get files in `ics` directory
 files = os.listdir('ics')
-# Use first file as base (to copy other properties)
-first = files[0]
-first_label = first[:first.rindex('.')]
-with open(f'ics/{first}') as f:
-    combined_calendar = icalendar.Calendar.from_ical(f.read())
-for event in combined_calendar.walk('VEVENT'):
-    event['SUMMARY'] = icalendar.vText(f"{event['SUMMARY']} ({first_label})")
-    event['CATEGORIES'] = icalendar.prop.vCategory([f'{OUTLOOK_EVENT_CATEGORIES[0]} category'])
 
-# Add remaining files
-for i, file in enumerate(files[1:]):
+# Map each file to a category
+assert(len(files) <= len(OUTLOOK_EVENT_CATEGORIES), 'Please add more categories!')
+category_map = {}
+for i, file in enumerate(files):
+    label = file[:file.rindex('.')]
+    category_map[label] = OUTLOOK_EVENT_CATEGORIES[i]
+
+# Get all events from all files
+events = {}
+
+for i, file in enumerate(files):
     label = file[:file.rindex('.')]
     with open(f'ics/{file}') as f:
         calendar = icalendar.Calendar.from_ical(f.read())
     for event in calendar.walk('VEVENT'):
-        event['SUMMARY'] = icalendar.vText(f"{event['SUMMARY']} ({label})")
-        event['CATEGORIES'] = icalendar.prop.vCategory([f'{OUTLOOK_EVENT_CATEGORIES[i+1]} category'])
-        combined_calendar.add_component(event)
+        if EventWrapper(event) in events:
+            events[EventWrapper(event)].append(label)
+        else:
+            events[EventWrapper(event)] = [label]
+
+# Use first file as base to transfer other properties
+with open(f'ics/{files[0]}') as f:
+    calendar = icalendar.Calendar.from_ical(f.read())
+
+# Remove existing events
+for event in calendar.walk('VEVENT'):
+    calendar.subcomponents.remove(event)
+
+# Add gathered events
+for wrapper, labels in events.items():
+    event = wrapper.event
+    label_text = '/'.join(labels)
+    event['SUMMARY'] = icalendar.vText(f"{event['SUMMARY']} ({label_text})")
+    event['CATEGORIES'] = icalendar.prop.vCategory([category_map[label] for label in labels])
+    calendar.add_component(event)
 
 # Create combined calendar file
 with open('combined.ics', 'wb') as f:
-    f.write(combined_calendar.to_ical())
+    f.write(calendar.to_ical())
