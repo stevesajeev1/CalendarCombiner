88f3d8e0cccd188f4308df43b35c76312f440706
Steve Sajeev <stevesajeev123@gmail.com>
2025-08-26 22:03:50 -0400

    Fix type issues

--- combiner.py
+++ combiner.py
@@ -1,6 +1,6 @@
 import os
 import icalendar
-from util import EventWrapper
+from util import ComponentWrapper
 
 # Add more categories as needed
 OUTLOOK_EVENT_CATEGORIES = ['Red category', 'Orange category', 'Yellow category', 'Green category', 'Blue category', 'Purple category']
@@ -11,45 +11,45 @@ OUTLOOK_EVENT_CATEGORIES = ['Red category', 'Orange category', 'Yellow category'
 files = os.listdir('ics')
 
 # Map each file to a category
-assert(len(files) <= len(OUTLOOK_EVENT_CATEGORIES), 'Please add more categories!')
+assert(len(files) <= len(OUTLOOK_EVENT_CATEGORIES), 'Please add more categories!') # type: ignore
 category_map = {}
 for i, file in enumerate(files):
     label = file[:file.rindex('.')]
     category_map[label] = OUTLOOK_EVENT_CATEGORIES[i]
 
-# Get all events from all files
-events = {}
+# Get all components from all files
+components = {}
 
 for i, file in enumerate(files):
     label = file[:file.rindex('.')]
     with open(f'ics/{file}') as f:
         calendar = icalendar.Calendar.from_ical(f.read())
-    for event in calendar.walk('VEVENT'):
-        if EventWrapper(event) in events:
-            events[EventWrapper(event)].append(label)
+    for component in calendar.walk('VEVENT'):
+        if ComponentWrapper(component) in components:
+            components[ComponentWrapper(component)].append(label)
         else:
-            events[EventWrapper(event)] = [label]
+            components[ComponentWrapper(component)] = [label]
 
 # Use first file as base to transfer other properties
 with open(f'ics/{files[0]}') as f:
     calendar = icalendar.Calendar.from_ical(f.read())
 
-# Remove existing events
-for event in calendar.walk('VEVENT'):
-    calendar.subcomponents.remove(event)
+# Remove existing components
+for component in calendar.walk('VEVENT'):
+    calendar.subcomponents.remove(component)
 
-# Add gathered events
-for wrapper, labels in events.items():
-    event = wrapper.event
+# Add gathered components
+for wrapper, labels in components.items():
+    component = wrapper.component
     label_text = '/'.join(labels)
-    event['SUMMARY'] = icalendar.vText(f"{event['SUMMARY']} ({label_text})")
-    event['CATEGORIES'] = icalendar.prop.vCategory([category_map[label] for label in labels])
+    component['SUMMARY'] = icalendar.vText(f"{component['SUMMARY']} ({label_text})")
+    component['CATEGORIES'] = icalendar.prop.vCategory([category_map[label] for label in labels])
 
     # Disable alarms
-    for alarm in event.walk('VALARM'):
-        event.subcomponents.remove(alarm);
+    for alarm in component.walk('VALARM'):
+        component.subcomponents.remove(alarm);
     
-    calendar.add_component(event)
+    calendar.add_component(component)
 
 # Create combined calendar file
 with open('combined.ics', 'wb') as f:
--- util.py
+++ util.py
@@ -1,23 +1,23 @@
 import typing
 import icalendar
 
-# Wrapper class to allow for comparison of icalendar.Event objects
-class EventWrapper:
-    def __init__(self, event: icalendar.Event):
-        self.event = event
+# Wrapper class to allow for comparison of icalendar.Component objects
+class ComponentWrapper:
+    def __init__(self, component: icalendar.Component):
+        self.component = component
     
     def __eq__(self, other):
-        return remove_unique(self.event) == remove_unique(other.event)
+        return remove_unique(self.component) == remove_unique(other.component)
 
     def __hash__(self):
         hash = 0
-        for subcomponent in self.event.subcomponents:
+        for subcomponent in self.component.subcomponents:
             if isinstance(subcomponent, typing.Hashable):
                 hash += subcomponent.__hash__()
         return hash
 
-def remove_unique(event: icalendar.Event):
-    copy = icalendar.Event.copy(event)
+def remove_unique(component: icalendar.Component):
+    copy = icalendar.Component.copy(component)
     copy.pop('DESCRIPTION')
     copy.pop('CATEGORIES')
     copy.pop('DTSTAMP')
